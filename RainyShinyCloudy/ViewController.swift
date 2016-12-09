fe777440b8e5bcd563dd347feb00ba80905a6f5f
Rafael Nicolett <rafaelnicolett@outlook.com>
2016-12-09 13:47:00 -0200

    Commit finish layout Storyboard with TableView and Cell

--- RainyShinyCloudy/ViewController.swift
+++ RainyShinyCloudy/ViewController.swift
@@ -8,11 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource   {
 
+    @IBOutlet weak var lblToday: UILabel!
+    @IBOutlet weak var lblTemperature: UILabel!
+    @IBOutlet weak var lblCity: UILabel!
+    @IBOutlet weak var imgTemperature: UIImageView!
+    @IBOutlet weak var lblStatusTemp: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellWeather", for: indexPath) as! WeatherTableViewCell
+            
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 }
 
---
+++ RainyShinyCloudy/WeatherTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherTableViewCell.swift
+//  RainyShinyCloudy
+//
+//  Created by Rafael Nicoleti on 09/12/16.
+//  Copyright © 2016 Rafael Nicoleti. All rights reserved.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imgTemperature: UIImageView!
+    @IBOutlet weak var lblTypeRain: UILabel!
+    @IBOutlet weak var lblDayWek: UILabel!
+    
+    @IBOutlet weak var lblTemperatureMin: UILabel!
+    @IBOutlet weak var lblTemperatureMax: UILabel!
+}
